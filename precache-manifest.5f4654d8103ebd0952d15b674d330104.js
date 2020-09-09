d18159b3f5ac4cfdd629c35fa73a02ac2d754640
unknown <christianglrza99@gmail.com>
2020-09-09 00:14:33 -0700

    Updates

---
+++ precache-manifest.5f4654d8103ebd0952d15b674d330104.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f07ac49b9c6c7974913be73b0740bb",
+    "url": "/ChristianGalarza/monsterFiltering/index.html"
+  },
+  {
+    "revision": "851698cddb028d2b8acb",
+    "url": "/ChristianGalarza/monsterFiltering/static/css/main.935157ea.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/ChristianGalarza/monsterFiltering/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ChristianGalarza/monsterFiltering/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "851698cddb028d2b8acb",
+    "url": "/ChristianGalarza/monsterFiltering/static/js/main.12459063.chunk.js"
+  },
+  {
+    "revision": "6c1e0a822db14a5f07bd",
+    "url": "/ChristianGalarza/monsterFiltering/static/js/runtime-main.66b1c007.js"
+  }
+]);
