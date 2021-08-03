eecbe8f689482430af0e16a6b22a621308b8e5ce
ChristianGlrz <christianglrza99@gmail.com>
2021-08-02 17:45:53 -0700

    Updates

---
+++ precache-manifest.d31d8dd45f3dffe521ae22bee48bba9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5669249911d24434f3b9532ca608b696",
+    "url": "/monsterFiltering/index.html"
+  },
+  {
+    "revision": "0793d50be583d86f00a3",
+    "url": "/monsterFiltering/static/css/main.935157ea.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsterFiltering/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterFiltering/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0793d50be583d86f00a3",
+    "url": "/monsterFiltering/static/js/main.1135a8b0.chunk.js"
+  },
+  {
+    "revision": "d4fd7679916ff3f2abf3",
+    "url": "/monsterFiltering/static/js/runtime-main.80f4f2a0.js"
+  }
+]);
